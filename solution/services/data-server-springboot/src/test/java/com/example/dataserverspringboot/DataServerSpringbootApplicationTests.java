a08c111785827276d9797a7d0b619bc7599f1591
Federico Collura <federico.collura@edu.unito.it>
2026-01-05 16:10:03 +0100

    Springboot project structure

---
+++ solution/services/data-server-springboot/src/test/java/com/example/dataserverspringboot/DataServerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataserverspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServerSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
