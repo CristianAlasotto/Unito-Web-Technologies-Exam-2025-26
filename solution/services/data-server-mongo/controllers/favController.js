65adfec6e6496fd59646ff0834f135210ca53bcd
davide <davide.correndo@edu.unito.it>
2026-01-23 17:34:39 +0100

    First APIs calls for favs

--- solution/services/data-server-mongo/controllers/favController.js
+++ solution/services/data-server-mongo/controllers/favController.js
@@ -1,11 +1,36 @@
-const Favs = require("../models/Favs");
-const axios = require('axios');
+const favService = require("../services/favService");
 
 exports.getFavs = async (req, res) => {
     try {
-        const favs = await Favs.find().limit(10);
+        const data = await favService.getAll(req.query);
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
 
-        res.json(favs);
+exports.getUserFavorites = async (req, res) => {
+    try {
+        const data = await favService.getByUser(req.params.username);
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.getAnimeFavCount = async (req, res) => {
+    try {
+        const count = await favService.getCountByAnime(req.params.animeId);
+        res.json({ anime_id: req.params.animeId, favorites_count: count });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.getPopularAnime = async (req, res) => {
+    try {
+        const data = await favService.getPopular(req.query.n);
+        res.json(data);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
--- solution/services/data-server-mongo/routes/index.js
+++ solution/services/data-server-mongo/routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const ratingController = require('../controllers/ratingController');
-const statController = require("../controllers/statController");
-const favController = require("../controllers/favController");
+const favController = require('../controllers/favController');
 
-router.get('/getratings', ratingController.getRatings);
-router.get('/getstats', statController.getStats);
-router.get('/getfavs', favController.getFavs);
+// Standard fetch with filters/pagination
+router.get('/favorites', favController.getFavs);
+
+// Changed getFavById to getUserFavorites to match your controller
+router.get('/favorites/user/:username', favController.getUserFavorites);
+
+// Statistics and analysis
+router.get('/favorites/anime/:animeId/count', favController.getAnimeFavCount);
+router.get('/favorites/popular', favController.getPopularAnime);
 
 module.exports = router;
