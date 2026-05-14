cc3fcbdeff55c65a84496c2b8d1be773c3503265
CristianAlasotto <alasotto82@gmail.com>
2026-05-14 15:52:38 +0200

    Jsdoc added to main-server-express address

--- solution/services/main-server-express/app.js
+++ solution/services/main-server-express/app.js
@@ -92,6 +92,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/jsdoc', express.static(path.join(__dirname, 'docs', 'jsdoc')));
 app.use('/vendor/axios', express.static(path.join(__dirname, 'node_modules', 'axios', 'dist')));
 
 app.use('/', indexRouter);
